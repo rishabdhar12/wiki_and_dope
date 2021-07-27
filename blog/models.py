58c53dd73d8314336949f1cd33f7253607af94f9
rishabdhar12 <rishabdhar12@gmail.com>
2021-07-27 23:44:54 +0530

    added navbar

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	published_at = models.DateTimeField(default=timezone.now)
+
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		ordering = ["-published_at"]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+
+from django.views.generic.list import ListView
+
+from .models import Post
 
 # Create your views here.
+
+class Home(ListView):
+	model = Post
+	context_object_name = 'posts'
+	template_name = 'blog/posts.html'
