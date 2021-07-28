8a650757a84eab5f571d2da1ceb3d20982c055d2
rishabdhar12 <rishabdhar12@gmail.com>
2021-07-28 17:46:42 +0530

    fized styling

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Post
 
@@ -11,3 +12,9 @@ class Home(ListView):
 	model = Post
 	context_object_name = 'posts'
 	template_name = 'blog/posts.html'
+
+
+class PostDetail(DetailView):
+	model = Post
+	context_object_name = "post"
+	template_name = 'blog/post.html'
