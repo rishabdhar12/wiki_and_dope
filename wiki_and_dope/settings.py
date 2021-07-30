71dd0e80372e42d09bc368b7562648d15e1c920a
rishabdhar12 <rishabdhar12@gmail.com>
2021-07-30 23:47:58 +0530

    fixed image upload

--- wiki_and_dope/settings.py
+++ wiki_and_dope/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 
@@ -132,7 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
